2ccee2a90bf8d88ce243043c78bd3831b2d37f20
LuisSempre <luis.silva@viannasempre.com.br>
2023-06-22 12:42:47 -0300

    Add filter genre

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [games, setGames] = useState<Array<Game>>([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState('');
 
   useEffect(() => {
     let timer: number;
@@ -70,9 +71,15 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredGames = games.filter((game) =>
-    game.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleGenreChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedGenre(event.target.value);
+  };
+
+  const filteredGames = games
+    .filter((game) =>
+      game.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .filter((game) => !selectedGenre || game.genre === selectedGenre);
 
   if (loading) {
     return (
@@ -82,19 +89,32 @@ const App = () => {
     );
   }
 
+  const genres = Array.from(new Set(games.map((game) => game.genre)));
+
   return (
     <div className='max-w-7xl mx-auto w-full h-full p-8'>
-      <div className='flex justify-center items-center flex-col'>
+      <div className='flex justify-center items-center flex-col w-full h-full'>
         <div>
           {errorMessage && <p>{errorMessage}</p>}
         </div>
         <div>
           <input
+          className='border rounded-lg'
             type='text'
             placeholder='Pesquisar por título'
             value={searchTerm}
             onChange={handleSearchChange}
           />
+        </div>
+        <div>
+          <select value={selectedGenre} onChange={handleGenreChange}className='border rounded-lg'>
+            <option value=''>Todos os gêneros</option>
+            {genres.map((genre) => (
+              <option value={genre} key={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
       <div className='grid lg:grid-cols-2 grid-cols-1 lg:p-8 p-8 xl:grid-cols-3 gap-8'>
