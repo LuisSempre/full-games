375ce10b5862444f15133990774487d34edaa39f
LuisSempre <luis.sempre@viannasempre.com.br>
2023-07-09 17:44:37 -0300

    Add @types/node

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 const Header: React.FC = () => {
     return (
-        <header className="flex justify-center items-center w-full bg-black p-8">
+        <header className="flex justify-center items-center w-full bg-gray-950 p-8">
             <Logo />
             <h1 className="text-gray-400 text-4xl font-modern">Lista de Jogos</h1>
         </header>
