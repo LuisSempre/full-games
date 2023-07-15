54e80ba4643e54cc5ccee6f444d6a75b62beaa4a
LuisSempre <luis.sempre@viannasempre.com.br>
2023-07-15 18:58:03 -0300

    nextConfig export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
    outDir: 'dist',
    images: {
       domains: ['www.freetogame.com'],
     },
- };
+}
+
+module.exports = nextConfig
