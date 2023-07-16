684484c522d2d274bd238e9144aa955851b7a622
LuisSempre <luis.sempre@viannasempre.com.br>
2023-07-16 20:58:10 -0300

    Up Form Mobile

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -225,14 +225,14 @@ const Games: FC = () => {
 
         <div className="grid lg:grid-cols-2 grid-cols-1 xl:grid-cols-3 gap-16 font-roboto">
           {currentGames.map((game) => (
-            <div className="relative z-10 shadow-lg ring-1 ring-indigo-500/10 rounded-3xl w-full hover:border-indigo-100 hover:border">
+            <div key={game.id} className="relative z-10 shadow-lg ring-1 ring-indigo-500/10 rounded-3xl w-full hover:border-indigo-100 hover:border">
               <div className="absolute -top-px left-1/2 -ml-24 flex h-[2px] w-48">
                 <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
                 <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
                 <div className="-ml-[100%] w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
                 <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
               </div>
-              <div className="relative bg-white px-4 py-10 sm:rounded-3xl sm:px-10 "  key={game.id}>
+              <div className="relative bg-white px-4 py-10 sm:rounded-3xl sm:px-10 "  >
     
                   <h2 className="font-semibold text-base text-indigo-500 p-2">
                     {game.title}
