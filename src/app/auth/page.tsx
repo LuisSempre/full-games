776eb156a4865f75779b0c1c9789fd04c37542c5
LuisSempre <luis.sempre@viannasempre.com.br>
2023-07-16 22:32:07 -0300

    Up Readme

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,7 +6,7 @@ function SignIn() {
   return (
     <Container className="flex flex-col items-center justify-center h-screen">
       <div className="relative z-10 shadow-lg ring-1 ring-indigo-500/10 rounded-xl hover:border hover:border-indigo-100">
-        <div className="absolute -bottom-px left-1/2 sm:-ml-48 -ml-24 flex h-[2px] sm:w-96 w-48">
+        <div className="absolute -bottom-px left-1/2 -ml-24 flex h-[2px] w-48">
           <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
           <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
           <div className="-ml-[100%] w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
