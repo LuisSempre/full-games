9b5ddb402ea8a520989ae21b8496205195ff6053
LuisSempre <luis.sempre@viannasempre.com.br>
2023-07-16 21:57:57 -0300

    Up Readme

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -19,7 +19,7 @@ const Navigation: FC = () => {
     return (
       <Container className="flex flex-col items-center justify-center">
         <div className="relative z-10 -mx-4 shadow-lg ring-1 ring-indigo-500/10 rounded-b-3xl w-full text-center hover:border hover:border-indigo-500">
-          <div className="absolute -bottom-px left-1/2 -ml-48 flex h-[2px] w-96">
+          <div className="absolute -bottom-px left-1/2 sm:-ml-48 flex h-[2px] sm:w-96 w-48 -ml-24">
             <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
             <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
             <div className="-ml-[100%] w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
@@ -51,7 +51,7 @@ const Navigation: FC = () => {
   return (
     <Container className="flex flex-col items-center justify-center">
       <footer className="relative z-10 -mx-4 shadow-lg ring-1 ring-indigo-500/10 rounded-b-3xl w-full text-center hover:border hover:border-indigo-100">
-        <div className="absolute -bottom-px left-1/2 -ml-48 flex h-[2px] w-96">
+        <div className="absolute -bottom-px left-1/2 sm:-ml-48 flex h-[2px] sm:w-96 w-48 -ml-24">
           <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
           <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
           <div className="-ml-[100%] w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
